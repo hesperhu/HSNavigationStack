fb97a03bfc6f7a41ca061ad5da55d366d33a8152
hu shouwen <hesper.hu@gmail.com>
2023-06-22 12:02:15 +0800

    Initial Commit

---
+++ HSNavigationStack/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HSNavigationStack
+//
+//  Created by hsw on 2023-06-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HSNavigationStack/HSNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSNavigationStackApp.swift
+//  HSNavigationStack
+//
+//  Created by hsw on 2023-06-22.
+//
+
+import SwiftUI
+
+@main
+struct HSNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
